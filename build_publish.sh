a2941418bf1f83e2aeade408104ed5ff173de34d
Mauricio Filho <ninoheavymetal@hotmail.com>
2023-02-04 20:07:10 -0300

    d

--- build_publish.sh
+++ build_publish.sh
@@ -1,4 +1,5 @@
-docker build --network=host -t registry.dubaismoke.net.br:5000/api-users:1.0.0 -f ./src/DubaiSmoke.Users.Api/Dockerfile .
+set IMAGENAME=registry.dubaismoke.net.br:5000/api-users:1.0.0
+docker build --network=host -t $IMAGENAME -f ./src/DubaiSmoke.Users.Api/Dockerfile .
 docker rm -f api-users
-docker run -d --name api-users -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always registry.dubaismoke.net.br:5000/api-users:1.0.0
-docker rmi -f registry.dubaismoke.net.br:5000/api-users:1.0.0
+docker run -d --name api-users -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always $IMAGENAME
+docker rmi -f $IMAGENAME
