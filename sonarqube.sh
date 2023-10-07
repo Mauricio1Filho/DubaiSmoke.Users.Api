f0f448a6fb0424e1e0518105cb149a8eadb9f3a1
Mauricio <mauriciocirinofilho@gmail.com>
2023-10-07 02:20:40 -0300

    sonarqube

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,14 +1,5 @@
-# Início da análise no SonarQube
-dotnet sonarscanner begin /k:"DubaiSmokeUsersApi" /d:sonar.login="sqp_e5048ddabe6ee243d80e386f2554e6f422c73d6c"
+dotnet sonarscanner begin /k:"DubaiSmokeUsersApi" /d:sonar.token="sqp_e5048ddabe6ee243d80e386f2554e6f422c73d6c" /d:sonar.language="cs" /d:sonar.cs.opencover.reportsPaths=src/DubaiSmoke.Users.Test/coverage.opencover.xml
 
-# Compilação do projeto .NET
-dotnet build --no-incremental
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
-# Execução dos testes com Cobertura de Código usando Coverlet
-coverlet .\src\DubaiSmoke.Users.Test\bin\Debug\net6.0\DubaiSmoke.Users.Test.dll --target "dotnet" --targetargs "test --no-build" -f=opencover -o="coverage.xml"
-
-# Fim da análise no SonarQube
-dotnet sonarscanner end /d:sonar.login="sqp_e5048ddabe6ee243d80e386f2554e6f422c73d6c"
-
-dotnet test ./src/DubaiSmoke.Users.Test/DubaiSmoke.Users.Test.csproj /p:ColectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=Coverage/
-reportgenerator "src/DubaiSmoke.Users.Test/Coverage/coverage.opencover.xml" "-targetdir:src/DubaiSmoke.Users.Test/Coverage"
+dotnet sonarscanner end /d:sonar.token="sqp_e5048ddabe6ee243d80e386f2554e6f422c73d6c"
