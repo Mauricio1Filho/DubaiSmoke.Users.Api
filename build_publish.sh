a0c56ac3f5edebedd4a25c394960c5a34d722819
Mauricio <mauriciocirinofilho@gmail.com>
2023-10-07 03:35:55 -0300

    commit

--- build_publish.sh
+++ build_publish.sh
@@ -1,4 +1,4 @@
-export IMAGENAME=registry.dubaismoke.net.br:5000/api-users:1.0.0
+export IMAGENAME=registry:2/api-users:1.0.2
 docker build --network=host -t $IMAGENAME -f ./src/DubaiSmoke.Users.Api/Dockerfile .
 docker rm -f api-users
 docker run -d --name api-users -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always $IMAGENAME
