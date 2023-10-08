2f8845f6a2ad19bb1a6d3a75f91383d45a6cf8ca
Mauricio <mauriciocirinofilho@gmail.com>
2023-10-07 21:07:54 -0300

    ajustes

--- build_publish.sh
+++ build_publish.sh
@@ -1,5 +1,6 @@
-export IMAGENAME=registry:2/api-users:1.0.2
+export ALIAS=api-users
+export IMAGENAME=registry:2/$ALIAS:1.0.2
 docker build --network=host -t $IMAGENAME -f ./src/DubaiSmoke.Users.Api/Dockerfile .
-docker rm -f api-users
+docker rm -f $ALIAS
 docker run -d --name api-users -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always $IMAGENAME
 docker rmi -f $IMAGENAME
